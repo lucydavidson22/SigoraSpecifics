e36fdd74c0659eb145ca71d1fa76e556a72ab23b
Lucy Davidson <dav18033@byui.edu>
2021-12-16 19:21:40 -0500

    fixing date input

--- home.js
+++ home.js
@@ -17,3 +17,11 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+// getting the date
+var field = document.querySelector('#today');
+var date = new Date();
+
+// Set the date
+field.value = date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString().padStart(2, 0) + 
+    '-' + date.getDate().toString().padStart(2, 0);
