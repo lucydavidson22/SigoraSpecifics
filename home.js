0765288bb02c5e562472b478606f926981cd5a50
Lucy Davidson <dav18033@byui.edu>
2021-12-16 19:15:03 -0500

    adding forms on second page

--- home.js
+++ home.js
@@ -1,20 +1,3 @@
-// const backdrop = document.querySelector('.backdrop');
-// const sideDrawer = document.querySelector('.mobile-nav');
-// const menuToggle = document.querySelector('#side-menu-toggle');
-
-// function backdropClickHandler() {
-//   backdrop.style.display = 'none';
-//   sideDrawer.classList.remove('open');
-// }
-
-// function menuToggleClickHandler() {
-//   backdrop.style.display = 'block';
-//   sideDrawer.classList.add('open');
-// }
-
-// backdrop.addEventListener('click', backdropClickHandler);
-// menuToggle.addEventListener('click', menuToggleClickHandler);
-
 // adding js for mobile nav
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
