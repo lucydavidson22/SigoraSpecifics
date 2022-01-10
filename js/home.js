823e2e6a28c0f2a4a0b9160d67a0525d37eaeee6
Lucy Davidson <lucydavidson@Lucys-MacBook-Air.local>
2022-01-10 15:18:04 -0500

    starting JS

--- js/home.js
+++ js/home.js
@@ -25,3 +25,22 @@ var date = new Date();
 // Set the date
 field.value = date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString().padStart(2, 0) + 
     '-' + date.getDate().toString().padStart(2, 0);
+
+// function knocksPerAnswer(){
+//     var knocks = getElementById("numKnocks").value;
+//     var answers = getElementById("numAnswers").value;
+//     var knockAns = knocks/answers;
+//     // document.getElementById("knocksAnswers").innerHTML = knocks/answers;
+
+//     var divKnockAnswers = document.getElementById('knocksAnswers');
+//     divKnockAnswers.innerHTML = divKnockAnswers.innerHTML + knockAns.value;
+// }
+
+function add_number() {
+               
+    var first_number = parseInt(document.getElementById("numKnocks").value);
+    var second_number = parseInt(document.getElementById("numAnswers").value);
+    var result = first_number/second_number;
+
+    document.getElementById("txtresult").value = result;
+}
